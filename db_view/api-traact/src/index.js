436e73b7139d33e6d1d6cfad9f6f6d9754efc446
darkvictor13 <01victorlmeida2001@hotmail.com>
2022-10-07 14:18:45 -0300

    sync

---
+++ db_view/api-traact/src/index.js
@@ -0,0 +1,85 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import { PostgrestClient } from "@supabase/postgrest-js";
+import { Router } from "itty-router";
+
+const router = Router();
+const client = new PostgrestClient(POSTGREST_ENDPOINT);
+
+router.get("/users", async (request) => {
+  const { data, error } = await client.from("users").select();
+  if (error !== null) {
+    return new Response(JSON.stringify(error), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  return new Response(JSON.stringify(data), {
+    headers: { "content-type": "application/json" },
+  });
+});
+
+router.get("/", async (request) => {
+  const { data, error } = await client.from("employees").select();
+  if (error !== null) {
+    return new Response(JSON.stringify(error), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  return new Response(JSON.stringify(data), {
+    headers: { "content-type": "application/json" },
+  });
+});
+
+/*
+router.get("/users", async (request) => {
+  const { data, error } = await client.from("users").select();
+  return new Response(JSON.stringify(data), {
+    headers: { "content-type": "application/json" },
+  });
+});
+
+router.get("/users/:id", async ({ params }) => {
+  console.log(params);
+  const { id } = params;
+  const { data, error } = await client.from("users").select().eq("id", id);
+  console.log(data);
+  const user = data.length ? data[0] : null;
+  return new Response(JSON.stringify({ user }), {
+    headers: { "content-type": "application/json" },
+  });
+});
+
+router.post("/users", async (request) => {
+  const userData = await request.json();
+  const { data, error } = await client.from("users").insert(userData);
+  const user = data.length ? data[0] : null;
+  return new Response(JSON.stringify({user}), {
+    headers: { "content-type": "application/json" },
+  });
+});
+*/
+
+router.all("*", () => new Response("Not Found", { status: 404 }));
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  console.log(POSTGREST_ENDPOINT);
+  return router.handle(request);
+}
---
+++ following_tutorial/README.md
@@ -0,0 +1,39 @@
+## Followed tutorials:
+- https://www.youtube.com/watch?v=inLOwovtqQM
+- https://developers.cloudflare.com/workers/tutorials/postgres
+
+## Run the worker:
+1. Clone this repository with submodules:
+```
+git clone --recurse-submodules <url>
+or
+git clone <url>
+git submodule update --init --recursive
+```
+2. Run the docker: 
+```
+cd CloudFlare-Workers-Test/following_tutorial/postgres-postgrest-cloudflared-example
+docker-compose up -d
+```
+3. Get the tunel from cloudflare
+- Get the output from thr docker
+```
+docker-compose logs --follow
+```
+- The docker-compose output should produce something like:
+```
+postgres-postgrest-cloudflared-example-cloudflared-1  | 2022-10-04T13:35:44Z INF +--------------------------------------------------------------------------------------------+
+postgres-postgrest-cloudflared-example-cloudflared-1  | 2022-10-04T13:35:44Z INF |  Your quick Tunnel has been created! Visit it at (it may take some time to be reachable):  |
+postgres-postgrest-cloudflared-example-cloudflared-1  | 2022-10-04T13:35:44Z INF |  https://url.trycloudflare.com                           |
+postgres-postgrest-cloudflared-example-cloudflared-1  | 2022-10-04T13:35:44Z INF +--------------------------------------------------------------------------------------------+
+```
+4. Deploy the worker:
+- Install wrangler2, my case `paru -S cloudflare-wrangler2`;
+- Login: `wrangler2 login`;
+- Set the url for the worker:
+```
+wrangler2 secret put POSTGREST_ENDPOINT
+```
+-> Enter a secret value: the_url_from_docker_logs
+- Change the account_id in wrangler.toml
+- publish the worker: `wrangler2 publish`
