9b86b4a2c93597cc54db9f09d48bdc6720526f26
darkvictor13 <01victorlmeida2001@hotmail.com>
2022-10-14 10:00:28 -0300

    creating a standard header response

--- db_view/api-traact/src/index.js
+++ db_view/api-traact/src/index.js
@@ -11,44 +11,39 @@
 import { PostgrestClient } from "@supabase/postgrest-js";
 import { Router } from "itty-router";
 
+const response_headers = new Headers({"Content-Type": "application/json"});
 const router = Router({ base: "/api" });
 const client = new PostgrestClient(POSTGREST_ENDPOINT);
 
 // Count all users in the database
 router.get("/count/users", async (request) => {
-  const { _ , error, count } = await client
+  const { _, error, count } = await client
     .from("users")
     .select("id", { count: "exact" });
 
   if (error !== null) {
     return new Response(JSON.stringify(error), {
       status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: response_headers,
     });
   }
   return new Response(JSON.stringify({ count }), {
     status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: response_headers,
   });
 });
 
 // Endpoint for documents analytics
-// Returns the number of documents edited, downloaded and uploaded separeted by day
+// Returns the number of documents edited, downloaded and uploaded separated by day
 router.get("/documents", async (request) => {
-  const {data, error} = await client
+  const { data, error } = await client
     .from("documents")
     .select("id, createdAt, updatedAt");
 
   if (error !== null) {
     return new Response(JSON.stringify(error), {
       status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: response_headers,
     });
   }
 
@@ -86,35 +81,47 @@ router.get("/documents", async (request) => {
       return acc;
     }, {});
 
-  return new Response(JSON.stringify({"created": created, "updated": updated}), {
+  return new Response(JSON.stringify({ created: created, updated: updated }), {
     status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: response_headers,
   });
 });
 
-
 router.get("/tasks/:status", async (request) => {
-  const status = request.params.status || 'DONE';
+  let response_status = 200;
+  let response_body = { status: "ok" };
+
+  const status = request.params.status;
+  if (
+    status !== "OPENED" &&
+    status !== "BLOCKED" &&
+    status !== "DOING" &&
+    status !== "DONE"
+  ) {
+    response_status = 400;
+    response_body = JSON.stringify({
+      error: "Invalid status",
+      hint: "Status must be one of OPENED, BLOCKED, DOING or DONE",
+    });
+    return new Response(response_body, {
+      status: response_status,
+      headers: response_headers,
+    });
+  }
   const { data, error } = await client
     .from("tasks")
     .select("tags, status, createdAt, updatedAt")
     .eq("status", status);
 
   if (error !== null) {
-    return new Response(JSON.stringify(error), {
-      status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    response_status = 500;
+    response_body = JSON.stringify(error);
+  } else {
+    response_body = JSON.stringify(data);
   }
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
+  return new Response(response_body, {
+    status: response_status,
+    headers: response_headers,
   });
 });
 
